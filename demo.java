683b209fe8b6492f7c1162906bba95fb1e4c6fe1
mohdanas-2002 <1mamas7396@gmail.com>
2024-12-12 08:35:01 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,18 @@
+class Demo
+
+{
+	public static void mainn(...)
+	{
+		s.o.print("hello world")
+		s.o.print("hello world")
+		s.o.print("hello world")
+		s.o.print("hello world")
+		s.o.print("hello world")
+	}
+}
+
+
+
+
+
+
